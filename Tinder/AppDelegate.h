fcf7b9f63acc56ceec27e7375ecd5e138a88beb2
Ivan Ruiz <ivanruizmonjo@gmail.com>
2014-08-25 20:56:13 -0500

    Initial Commit

---
+++ Tinder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tinder
+//
+//  Created by Ivan Ruiz Monjo on 25/08/14.
+//  Copyright (c) 2014 ivan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tinder/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Tinder
+//
+//  Created by Ivan Ruiz Monjo on 25/08/14.
+//  Copyright (c) 2014 ivan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
